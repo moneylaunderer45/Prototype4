eddc13f19c895f7998777b1b0d5942527e52e8ae
moneylaunderer45 <146860802+moneylaunderer45@users.noreply.github.com>
2023-10-24 09:54:07 -0700

    4.2

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint; 
     // Start is called before the first frame update
     void Start()
     {
-        
+     playerRb = GetComponent<Rigidbody>();
+     focalPoint = GameObject.Find("Focal Point"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     float forwardInput = Input.GetAxis("Verticle");
+     playerRb.AddForce(focalPoint.transform.forward * speed* forwardInput);
     }
 }
